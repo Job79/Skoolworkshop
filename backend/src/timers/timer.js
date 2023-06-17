23a2a3fdd5c6ca3e831a04e0fc70241d5ca0de62
kgithcoy <anhtuannguyen.2004@gmail.com>
2023-06-17 23:12:52 +0200

    refactor: made email send once every 24h

--- backend/src/timers/timer.js
+++ backend/src/timers/timer.js
@@ -35,40 +35,51 @@ const pushSubscription = {
     }
 }
 
+let lastSent = null;
+
 setInterval(async () => {
-    const startDate = new Date()
-    const endDate = new Date()
-    endDate.setFullYear(startDate.getFullYear() + 1)
+    const now = new Date();
+    
+    const startDate = new Date();
+    const endDate = new Date();
+    endDate.setFullYear(startDate.getFullYear() + 1);
 
-    const calendar = await calendarService.fetchCalendar(startDate, endDate)
-    const requiredStock = await calendarService.calculate(calendar)
-    const productsToOrder = await timerService.fetchProductsToOrder(requiredStock)
+    const calendar = await calendarService.fetchCalendar(startDate, endDate);
+    const requiredStock = await calendarService.calculate(calendar);
+    const productsToOrder = await timerService.fetchProductsToOrder(requiredStock);
 
-    let productsToOrderMessage = productsToOrder.map(product => {
-        return `Product ID: ${product.id}, Order Quantity: ${product.orderQuantity}`
-    }).join('\n')
+    // Check if stock is low and if an email hasn't been sent in the last 24 hours
+    if (productsToOrder.length > 0 && (!lastSent || (now - lastSent) >= (1000 * 60 * 60 * 24))) {
+        let productsToOrderMessage = productsToOrder.map(product => {
+            return `Product ID: ${product.id}, Order Quantity: ${product.orderQuantity}`
+        }).join('\n');
 
-    const mailOptions = {
-        from: 'nlskoolworkshop@gmail.com',
-        to: 'info@skoolworkshop.nl',
-        subject: 'Products to Order',
-        text: productsToOrderMessage
+        const mailOptions = {
+            from: 'nlskoolworkshop@gmail.com',
+            to: 'info@skoolworkshop.nl',
+            subject: 'Products to Order',
+            text: productsToOrderMessage
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log('Email sent: ' + info.response)
+            }
+        });
+
+        let message = JSON.stringify({
+            title: 'Products to Order',
+            body: productsToOrderMessage
+        });
+
+        webpush.sendNotification(pushSubscription, message);
+
+        // Update the lastSent time to now
+        lastSent = now;
     }
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error)
-        } else {
-            console.log('Email sent: ' + info.response)
-        }
-    })
-
-    let message = JSON.stringify({
-        title: 'Products to Order',
-        body: productsToOrderMessage
-    })
-
-    webpush.sendNotification(pushSubscription, message)
-
 }, 1000 * 60 * 60)
+
 
