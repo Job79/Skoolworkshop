0e56656a08f65608d223bf9ab444434c1a6d04db
Job79 <job@plabble.org>
2023-06-17 10:02:05 +0200

    fix: use close button for scanner

--- frontend/src/views/Scan.vue
+++ frontend/src/views/Scan.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { useProductStore } from '../store/productStore.js'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { StreamBarcodeReader } from 'vue-barcode-reader'
+import {useProductStore} from '../store/productStore.js'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {StreamBarcodeReader} from 'vue-barcode-reader'
 import router from '../router/router.js'
-import { useRoute } from 'vue-router'
+import {useRoute} from 'vue-router'
 
 const productStore = useProductStore()
 productStore.fetch()
@@ -11,30 +11,30 @@ productStore.fetch()
 const route = useRoute()
 const action = route.params.action
 
-function onDecode (result) {
-    const product = productStore.findCode(result)
-    if (!product) {
-        throw Error('unknown product')
-    }
+function onDecode(result) {
+  const product = productStore.findCode(result)
+  if (!product) {
+    throw Error('unknown product')
+  }
 
-    if (action === 'redirect') {
-        router.push(`/products/${product.id}`)
-    } else {
-        router.push(`/products/${product.id}`)
-    }
+  if (action === 'redirect') {
+    router.push(`/products/${product.id}`)
+  } else {
+    router.push(`/products/${product.id}`)
+  }
 }
 </script>
 
 <template>
-    <div class="row box-header">
-    <div class="d-flex align-items-center m-0" style="width: min-content">
-      <a class="btn p-2 bg-secondary hover-darken" @click="$router.back()">
-        <font-awesome-icon :icon="['fas', 'caret-left']" class="fa-xl" style="width: 24px"/>
-      </a>
-    </div>
-
+  <div class="row box-header">
     <div class="col d-flex align-items-center">
       <h3 class="m-0">Scan A QR / Barcode</h3>
+    </div>
+
+    <div class="col-4 d-flex align-items-center justify-content-end">
+      <a class="btn p-2 bg-secondary hover-darken" @click="$router.back()">
+        <font-awesome-icon :icon="['fas', 'x']" class="fa-1x" style="width: 24px"/>
+      </a>
     </div>
   </div>
 
